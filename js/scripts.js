5893e09b7b607d9e3d9f3bc3453e5ea8033383d8
itsdlew <Davidlws27@gmail.com>
2021-07-20 12:55:40 -0700

    first 3 pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-// alert("Hello world");
-let favoritefood = "BBQ";
-document.write(favoritefood);
+let pokemonList=[
+  {
+      name: "Bulbasaur",
+      height: 0.7,
+      types: ["grass", "poison"]
+},{
+      name: "Ivysaur",
+      height: 1,
+      types: ["grass","poison"]
+},{
+      name: "Venusaur",
+      height: 2,
+      types: ["grass","poison"]
+}];
