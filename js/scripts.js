e9476c0abb8d60d6c7ec67e4d35879fcfbf77869
itsdlew <Davidlws27@gmail.com>
2021-08-24 12:57:33 -0700

    edit code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
-// alert("Hello world");
-let favoritefood = "BBQ";
-document.write(favoritefood);
+  let pokemonList=[
+    {
+        name: "Bulbasaur",
+        height: 0.7,
+        types: ["grass", "poison"]
+    },{
+        name: "Ivysaur",
+        height: 1,
+        types: ["grass","poison"]
+    },{
+        name: "Venusaur",
+        height: 2,
+        types: ["grass","poison"]
+    }];
+    pokemonList.forEach(item =>{
+      console.log (item)
+    })
