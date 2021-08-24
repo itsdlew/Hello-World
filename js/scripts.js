bee433ff1f39ff090b83300f4417d8a535c05039
itsdlew <Davidlws27@gmail.com>
2021-08-24 12:53:55 -0700

    1.5 changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
-// alert("Hello world");
-let favoritefood = "BBQ";
-document.write(favoritefood);
+let pokemonRepository= (function(){
+  let pokemonList=[
+    {
+        name: "Bulbasaur",
+        height: 0.7,
+        types: ["grass", "poison"]
+    },{
+        name: "Ivysaur",
+        height: 1,
+        types: ["grass","poison"]
+    },{
+        name: "Venusaur",
+        height: 2,
+        types: ["grass","poison"]
+    }];
+    pokemonList.forEach(item =>{
+      console.log (item)
+    })
+    function getall(){
+      return pokemonList;
+    }
+    function add(newpokemon){
+      pokemonList.push(newpokemon)
+    }
+    return  {
+      getall: getall,
+      add:add
+    }
+
+})()
+pokemonRepository.getall().forEach(item=>{
+  console.log (item)
+})
